e2a00b7448e99da9cd193bb035727776c383e0a3
suchethaleonidou <Suchethao>
2023-03-20 17:53:06 -0400

    api documentation added

--- realestate/app.js
+++ realestate/app.js
@@ -20,49 +20,6 @@ app.get('/airbnb', async(req, res) => {
     res.json(allAirbnb)
 
 })
-/**
-* @api {get} /airbnb/ Get all airbnbs
-* @apiName GetAirbnbs
-* @apiGroup Airbnbs
-
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
-
 
 //Example 1: 
 // use ID from Request and get the document from AirBnb collection
@@ -85,51 +42,7 @@ app.get('/airbnb/:id', async(req, res) => {
         res.json(err)
     }
 })
-/**
-* @api {get} /airbnb/:id Get airbnb by Id
-* @apiName GetAirbnb by Id
-* @apiGroup Airbnbs
 
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
-
-*/
 //
 // Example 2: If filtering based on certain parameter like Name and then matching Dogpark location, we can use
 // the concept of joins (MongoDB Aggregation)
@@ -158,247 +71,18 @@ app.get('/airbnb/name/:name', async(req, res) => {
 
 })
 
-/**
-* @api {get} /airbnb/:id Get airbnb by Id
-* @apiName GetAirbnb by Id
-* @apiGroup Airbnbs
-
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
-
 app.post('/airbnb', async(req, res) => {
     const airbnb = await Airbnb.create(req.body)
     res.json(airbnb)
 })
 
-/**
-* @api {get} /airbnb/:id Get airbnb by Id
-* @apiName GetAirbnb by Id
-* @apiGroup Airbnbs
 
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
 
 app.get('/dogparks', async(req, res) =>  {
     let allDogParks = await DogPark.find({})
     res.json(allDogParks)
 })
 
-/**
-* @api {get} /airbnb/:id Get airbnb by Id
-* @apiName GetAirbnb by Id
-* @apiGroup Airbnbs
-
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
-app.put('/airbnb/:id', async(req, res)=> {
-    let airbnbs = await Airbnb.findByIdAndUpdate(
-        req.params.id,
-        {$set: req.body},
-        {new: true}
-    )
-    res.json(Airbnb)
-})
-
-/**
-* @api {get} /airbnb/:id Get airbnb by Id
-* @apiName GetAirbnb by Id
-* @apiGroup Airbnbs
-
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
-
-app.delete('/houses/:id', async(req, res) => {
-    let airbnb = await Airbnb.findOneAndRemove({_id: req.params.id})
-    res.json(airbnb)
-  })
-/**
-* @api {get} /airbnb/:id Get airbnb by Id
-* @apiName GetAirbnb by Id
-* @apiGroup Airbnbs
-
-* @apiSuccess {String} _id Airbnb id
-* @apiSuccess {String} name Airbnb name
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} neighbourhoodGroup Airbnb neighborhoodGroup
-* @apiSuccess {String} neighbourhood Airbnb neighbourhood
-* @apiSuccess {String} roomType Airbnb roomType
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {Number} price Airbnb price
-* @apiSuccess {Number} minimumNights Airbnb minimumNights
-* @apiSuccess {String} hostName Airbnb hostName
-* @apiSuccess {String} v Airbnb v
-* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
-* @apiSuccessExample Successful Response:
- * [
- * {
- *      "_id": "64161f12b93cd726a1e13725",
- *      "name": "Exit Glacier Trail",
- *      "area_name": "Kenai Fjords National Park",
- *      "city_name": "Seward",
- *      "state_name": "Alaska",
- *      "popularity": 17.7821,
- *      "length": 2896.812,
- *      "elevation_gain": 81.9912,
- *      "difficulty_rating": 1,
- *      "route_type": "out and back",
- *      "avg_rating": 4.5,
- *      "num_reviews": 224,
- *      "features": [
- *          "['dogs-no', 'partially-paved', 'views', 'wildlife']"
- *      ],
- *      "activities": [
- *          "['hiking', 'walking']"
- *      ],
- *      "__v": 0
- *      },
- *  ...
- * ]                   
-*/
 
 mongoose.connect(
     "mongodb://localhost:27017/realestate"
@@ -408,5 +92,149 @@ app.listen(3000, () => {
     console.log('running on port 3000')
 })
 
+//API DOCS
 
+/**
+* @api {GET} /airbnb/ Get all airbnbs
+* @apiName GetAirbnbs
+* @apiGroup Airbnbs
 
+* @apiSuccess {String} _id airbnb id
+* @apiSuccess {String} name airbnb name
+* @apiSuccess {String} host_name Airbnb host name
+* @apiSuccess {String} neighbourhood_group Airbnb borough
+* @apiSuccess {String} neighbourhood Airbnb neighbourhood
+* @apiSuccess {String} room_type Airbnb room type
+* @apiSuccess {Number} price Airbnb price
+* @apiSuccess {Number} minimum_nights Airbnb minimum nights
+* @apiSuccess {Number} __v Airbnb v
+* @apiSuccessExample Successful Response:
+
+* [
+ * {
+        "_id": "6418cea32802a14b7e9127b6",
+        "name": "Clean & quiet apt home by the park",
+        "host_name": "John",
+        "neighbourhood_group": "Brooklyn",
+        "neighbourhood": "Kensington",
+        "room_type": "Private room",
+        "price": 149,
+        "minimum_nights": 1,
+        "__v": 0
+ *      },
+ *  ...
+ * ]                   
+*/
+
+/**
+* @api {get} /airbnb/:id Get airbnb by Id
+* @apiName GetAirbnb by Id
+* @apiGroup Airbnbs
+
+* @apiParam {String} _id airbnb id
+
+* @apiSuccess {String} _id airbnb id
+* @apiSuccess {String} name airbnb name
+* @apiSuccess {String} host_name airbnb host name
+* @apiSuccess {String} neighbourhood_group airbnb neighborhood group
+* @apiSuccess {String} neighbourhood airbnb neighbourhood
+* @apiSuccess {String} room_type airbnb roomType
+* @apiSuccess {Number} price airbnb price
+* @apiSuccess {Number} minimum_nights Airbnb minimum nights
+* @apiSuccess {Number} __v Airbnb v
+* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
+
+* @apiSuccessExample Successful Response:
+ * [
+ *{
+    "_id": "6418cea32802a14b7e9127b6",
+    "name": "Clean & quiet apt home by the park",
+    "host_name": "John",
+    "neighbourhood_group": "Brooklyn",
+    "neighbourhood": "Kensington",
+    "room_type": "Private room",
+    "price": 149,
+    "minimum_nights": 1,
+    "__v": 0,
+    "dogParks": [
+        {
+            "_id": "6418cea82802a14b7e92a5bb",
+            "Name": "Henry Hudson Park Off-Leash Area",
+            "neighbourhood": "Brooklyn",
+            "Address": "Kappock Street and Independence Avenue",
+            "DogRuns_Type": "Off-Leash",
+            "Accessible": "N",
+            "__v": 0
+        },
+    ]          
+}]       
+*/
+/**
+@api {GET} /airbnb/:name/:name Get airbnb by it's name
+* @apiName GetAirbnbByName
+* @apiGroup Airbnbs
+
+ * @apiParam {String} name Name of the Airbnb
+
+* @apiSuccess {String} _id airbnb id
+* @apiSuccess {String} name airbnb name
+* @apiSuccess {String} host_name airbnb host name
+* @apiSuccess {String} neighbourhood_group airbnb neighborhood group
+* @apiSuccess {String} neighbourhood airbnb neighbourhood
+* @apiSuccess {String} room_type airbnb roomType
+* @apiSuccess {Number} price airbnb price
+* @apiSuccess {Number} minimum_nights Airbnb minimum nights
+* @apiSuccess {Number} __v Airbnb v
+* @apiSuccess {[Objects]} dogParks Array of parks objects neighboring Airbnb
+* @apiSuccessExample Successful Response:
+ * [
+    {
+        "_id": "6418cea32802a14b7e9127b6",
+        "name": "Clean & quiet apt home by the park",
+        "host_name": "John",
+        "neighbourhood_group": "Brooklyn",
+        "neighbourhood": "Kensington",
+        "room_type": "Private room",
+        "price": 149,
+        "minimum_nights": 1,
+        "__v": 0,
+        "dogParks": [
+            {
+                "_id": "6418cea82802a14b7e92a5bb",
+                "Name": "Henry Hudson Park Off-Leash Area",
+                "neighbourhood": "Brooklyn",
+                "Address": "Kappock Street and Independence Avenue",
+                "DogRuns_Type": "Off-Leash",
+                "Accessible": "N",
+                "__v": 0
+            },
+ *  ...
+ * ]                   
+*/
+
+/**
+@api {get} /dogparks Get all dog parks
+* @apiName GetDogparks
+* @apiGroup Dogparks
+
+* @apiSuccess {String} _id dogparks id
+* @apiSuccess {String} Name dogparks name
+* @apiSuccess {String} neighbourhood dogparks neighbourhood
+* @apiSuccess {String} address dogparks address
+* @apiSuccess {String} DogRuns_Type dogparks type
+* @apiSuccess {Number} Accessible dogparks wheelchair_accessibility
+* @apiSuccess {Number} __v dogparks v
+* @apiSuccessExample Successful Response:
+ * [
+   {
+        "_id": "6418cea82802a14b7e92a5bd",
+        "Name": "Joseph Rodham Drake Park Off-Leash Area",
+        "neighbourhood": "Manhattan",
+        "Address": null,
+        "DogRuns_Type": "Off-Leash",
+        "Accessible": "N",
+        "__v": 0
+    },
+ *  ...
+ * ]                   
+*/
