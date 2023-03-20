26040ba7e60ff1e00fa9224acebc1ff0fe298be7
suchethaleonidou <Suchethao>
2023-03-20 13:15:39 -0400

    adding new de[enencies for api documentation]

--- realestate/db/connection.js
+++ realestate/db/connection.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://127.0.0.1:27017/realestate', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1:27017/realestate', { useNewUrlParser: true }).then(
+    () => {
+        console.log("Database successfully connected");
+    }
+)
 
 export default mongoose;
